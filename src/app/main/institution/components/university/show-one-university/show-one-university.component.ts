48b9dcd51b4898b8ad6977c9861d780130e7721f
EmmanuelMezaMorales <65479091+edejesus20@users.noreply.github.com>
2022-01-30 22:05:43 -0500

    Componente Universidad (crear -editar - mostrar - mostrarOne - delete)

--- src/app/main/institution/components/university/show-one-university/show-one-university.component.ts
+++ src/app/main/institution/components/university/show-one-university/show-one-university.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MessageService, PrimeNGConfig } from 'primeng/api';
+import { UniversityService } from 'src/app/core/services/institution/university.service';
+import { UniversityI } from 'src/app/models/institution/university';
+const translate = require('translate');
 @Component({
   selector: 'app-show-one-university',
   templateUrl: './show-one-university.component.html',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowOneUniversityComponent implements OnInit {
 
-  constructor() { }
+  public mostrar:number=3;
+  public tabla:boolean=true;
+  displayMaximizable2:boolean=false
+  blockSpecial: RegExp = /^[^<>*!]+$/ 
+  private id:number=0
+  constructor(
+    private universityService: UniversityService,
+    private primengConfig: PrimeNGConfig,
+    private router: Router,
+    private messageService:MessageService
+  ) { }
 
   ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+  getOneCntAccount(id:number) {
+    this.universityService.getItem(id).subscribe((cnt_groupFromApi) => {
+      // this.cnt_account = cnt_groupFromApi.account;
+      this.displayMaximizable2=true
+      this.tabla = false
+      //console.log(this.cnt_group);
+    }, error => console.error(error));
   }
 
+  public volver(event: Event){
+    event.preventDefault
+    this.tabla = true
+    this.displayMaximizable2 = false
+    //console.log(event)
+  }
+
+  ngOnDestroy() {
+    this.tabla = true
+    this.displayMaximizable2 = false
+  }
+  actualizar(id: number){
+    console.log(id)
+    this.getOneCntAccount(id)
+  }
 }
--- src/app/models/institution/university.ts
+++ src/app/models/institution/university.ts
@@ -1,7 +1,10 @@
+import { FacultyI } from "./faculty";
+
 export interface UniversityI {
     id?: number;
     name: string;
     nit: string;
     addres: string;
     createdAt?:string
+    Faculties?:FacultyI[]
 }
